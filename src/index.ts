5716604e36bae852321cb56840b4fa2dbb70af8f
Karina P <karina_pineda97@hotmail.com>
2024-07-19 20:11:46 +0200

    feat:update

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,6 @@
-import { BooksController } from "./controllers/book.controller";
-import { RequestLoginBooks } from "./models/bookIRequest";
-import { ResponseLoginBooks } from "./models/bookIResponse";
 
+document.addEventListener('DOMContentLoaded', async () => {
 
-async function main(): Promise <void> {
-    
-    const dataToLogin:RequestLoginBooks={
-        email: 'prueba@prueba.pru',
-        password: 'C0ntr4S3gu++r4'
-    }
+});
 
-    const booksController:BooksController = new BooksController('http://190.147.64.47:5155/');
-
-    try {
-        const resultLogin:ResponseLoginBooks=await booksController.postLogin(dataToLogin);
-            console.log(resultLogin);       
-    } catch (e) {
-        console.log(` =( : ${e})`);
-    }
-
-}
-main();
+const login=document.querySelector('.section-login');
--- src/views/components/login.ts
+++ src/views/components/login.ts
@@ -1,1 +1,22 @@
-const login=document.querySelector('.section-login');
+import { BooksController } from "../../controllers/book.controller";
+import { RequestLoginBooks } from "../../models/bookIRequest";
+import { ResponseLoginBooks } from "../../models/bookIResponse";
+
+async function main(): Promise <void> {
+    
+    const dataToLogin:RequestLoginBooks={
+        email: 'prueba@prueba.pru',
+        password: 'C0ntr4S3gu++r4'
+    }
+
+    const booksController:BooksController = new BooksController('http://190.147.64.47:5155/');
+
+    try {
+        const resultLogin:ResponseLoginBooks=await booksController.postLogin(dataToLogin);
+            console.log(resultLogin);       
+    } catch (e) {
+        console.log(` =( : ${e})`);
+    }
+
+}
+main();
